6b94d4826a120b0e8539a37d0dc55c864479a1b0
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-11-21 22:36:28 +0100

    add lesson 5 progress
    Add portal system with prefabs, scripts, and shader
    
    Introduced a portal system by adding Portal, PortalCamera, and PortalTeleport scripts, a Portal prefab, and a custom ScreenCutoutShader. Updated SampleScene to include portal prefabs and made minor material and transform adjustments. Renamed GreenMaterial 1 to GoldMaterial.

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Portal otherPortal;
+    [SerializeField] Material material;
+
+    public Camera myCamera;
+
+    public Transform renderSurface;
+    public Transform portalCollider;
+
+    private GameObject player;
+    private PortalTeleport portalTeleport;
+    private PortalCamera portalCamera;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        portalTeleport = portalCollider.GetComponent<PortalTeleport>();
+        portalTeleport.player = player.transform;
+        portalTeleport.receiver = otherPortal.portalCollider;
+
+        portalCamera = myCamera.GetComponent<PortalCamera>();
+        portalCamera.playerCamera = player.GetComponentInChildren<Camera>().transform;
+    }
+}
